6bd18b21780882db97213d8de4e967b2bf9e747e
Bryony Watson <bryonywatson1@googlemail.com>
2016-11-30 12:35:18 +0000

    configuring the rack environement

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,6 @@
 require 'data_mapper'
 require "dm-postgres-adapter"
+require './app/app'
 class Link
 
 	include DataMapper::Resource
@@ -9,7 +10,6 @@ class Link
 	property :title, String
 
 end
-
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test")
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
--- config.ru
+++ config.ru
@@ -1,4 +1,7 @@
 require 'rubygems'
+require './app/app'
 require File.join(File.dirname(__FILE__), 'app.rb')
+
+set :environment, :development
 
 run BookmarkManager
